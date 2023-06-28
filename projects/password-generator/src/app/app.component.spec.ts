4200ff9a4b6f72c3f91e241827432424e70b6499
mviadieu <m.viadieu@gmail.com>
2023-06-28 17:31:02 +0200

    Unit Test to password-generator

---
+++ projects/password-generator/src/app/app.component.spec.ts
@@ -0,0 +1,55 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+  // le beforeEach va permettre de refactoriser. On relance le test bed avant chaque test.
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    }).compileComponents(); // permet de lancer un framework virtuel TestBed et de compiler les composants html et css
+    fixture = TestBed.createComponent(AppComponent); // le fixture est un navigature virtuel qui permet de voir le app component
+    fixture.autoDetectChanges(); //lance un cycle de détection de changment automatique à chaque action. (ex click button, MAJ interpolation etc.)
+
+    component = fixture.componentInstance; // refacto
+  });
+
+  it('should title be : 🔐 Générer un mot de passe fort 💪', async () => {
+    const h1 = fixture.nativeElement.querySelector('h1');
+    expect(h1.textContent).toBe('🔐 Générer un mot de passe fort 💪');
+  });
+
+  it('should article be initialize when page is loaded', async () => {
+    const article = fixture.nativeElement.querySelector('article');
+    expect(article.textContent).toBe('Cliquer sur le bouton "Générer"');
+  });
+
+  it('should change message when user clicks generate button', async () => {
+    const button = fixture.nativeElement.querySelector('button');
+
+    button.click();
+
+    const article = fixture.nativeElement.querySelector('article');
+    expect(article.textContent).toBe('MON_MOT_DE_PASSE');
+  });
+
+  it('should update settings when user clicks switch buttons', async () => {
+    fixture.nativeElement.querySelector('#uppercases').click();
+    expect(component.uppercases).toBe(true);
+
+    fixture.nativeElement.querySelector('#symbols').click();
+    expect(component.symbols).toBe(true);
+
+    fixture.nativeElement.querySelector('#numbers').click();
+    expect(component.numbers).toBe(true);
+  });
+
+  it('should slide bar return expected written value', async () => {
+    // Exemple sur les élements de type 'input'
+    const lengh = fixture.nativeElement.querySelector('#lengh');
+    lengh.value = 33;
+    lengh.dispatchEvent(new Event('input'));
+    expect(component.lengh).toBe(33);
+  });
+});
--- projects/password-generator/src/app/app.component.ts
+++ projects/password-generator/src/app/app.component.ts
@@ -4,17 +4,28 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <div class="container">
-      <h1>Générer un mot de passe fort 💪 🔐</h1>
+      <h1>🔐 Générer un mot de passe fort 💪</h1>
       <div class="grid">
         <div>
           <h3>Votre futur mot de passe:</h3>
-          <article>Cliquer sur le bouton "générer" ...</article>
+          <article>{{ message }}</article>
         </div>
         <div>
-          <label for="lenght">Longueur du mot de passe:</label>
-          <input id="lenght" type="range" min="10" max="50" name="lenght" />
+          <label for="lenght">Longueur du mot de passe: {{ lengh }}</label>
+          <input
+            id="lengh"
+            type="range"
+            min="10"
+            max="50"
+            name="lengh"
+            (input)="OnChangeLengh($event)"
+          />
           <label>
             <input
+              #uppercaseInput
+              (change)="
+                OnChangeSettings(uppercaseInput.name, uppercaseInput.checked)
+              "
               role="switch"
               id="uppercases"
               type="checkbox"
@@ -23,22 +34,69 @@ import { Component } from '@angular/core';
             Contiendra des majuscules
           </label>
           <label>
-            <input role="switch" id="numbers" type="checkbox" name="numbers" />
+            <input
+              #numbersInput
+              (change)="
+                OnChangeSettings(numbersInput.name, numbersInput.checked)
+              "
+              role="switch"
+              id="numbers"
+              type="checkbox"
+              name="numbers"
+            />
             Contiendra des nombres
           </label>
           <label>
-            <input role="switch" id="symbols" type="checkbox" name="symbols" />
+            <input
+              #symbolsInput
+              (change)="
+                OnChangeSettings(symbolsInput.name, symbolsInput.checked)
+              "
+              role="switch"
+              id="symbols"
+              type="checkbox"
+              name="symbols"
+            />
             Contiendra des caractères spéciaux
           </label>
           <hr />
-          <button>Générer</button>
-          <button>Copier le mot de passe</button>
+          <button (click)="onClickGenerate()">Générer</button>
+          <button class="outline">Copier le mot de passe</button>
         </div>
       </div>
     </div>
   `,
-  styles: [''],
+  styles: [],
 })
 export class AppComponent {
-  title = 'password-generator';
+  message = 'Cliquer sur le bouton "Générer"';
+  lengh = 50;
+  uppercases = false;
+  numbers = false;
+  symbols = false;
+
+  OnChangeSettings(settingName: string, settingValue: boolean) {
+    if (
+      settingName !== 'uppercases' &&
+      settingName !== 'numbers' &&
+      settingName !== 'symbols'
+    ) {
+      return;
+    }
+    this[settingName] = settingValue;
+  }
+
+  OnChangeLengh(event: Event) {
+    this.lengh = +(event.target as HTMLInputElement).value; // le + permet la conversion en number
+  }
+
+  onClickGenerate() {
+    this.message = 'MON_MOT_DE_PASSE';
+    console.table({
+      uppercase: this.uppercases,
+      numbers: this.numbers,
+      symbols: this.symbols,
+      lengh: this.lengh,
+    });
+  }
 }
